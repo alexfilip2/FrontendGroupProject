28d84f36d1eaf98bcdf1f63331c43a89fefa0585
Alex Filip <acf61@cam.ac.uk>
2018-02-11 01:10:49 +0000

    "new menu"

--- server.py
+++ server.py
@@ -1,11 +1,10 @@
 import   os
 import csv
-from backendController import *
 
 try: # Windows needs stdio set for binary mode.
-    import msvcrt
-    msvcrt.setmode (0, os.O_BINARY) # stdin  = 0
-    msvcrt.setmode (1, os.O_BINARY) # stdout = 1
+     import msvcrt
+     msvcrt.setmode (0, os.O_BINARY) # stdin  = 0
+     msvcrt.setmode (1, os.O_BINARY) # stdout = 1
 except ImportError:
     pass
 
@@ -14,30 +13,110 @@ from flask import Flask, jsonify, render_template, request
 app = Flask(__name__)
 
 import urllib.request, json
-
+#data for the first tile plot
 url = urllib.request.urlopen("https://cdn.rawgit.com/highcharts/highcharts/v6.0.5/samples/data/usdeur.json")
-graph_data = json.loads(url.read().decode())
+dust_exposure_data = json.loads(url.read().decode())
+#data for the risk (when engines will fail on a horizontal stacked bar) plot
+risk_graph_data = [ {
+        "category": "Aircraf1",
+        "segments": [ {
+            "start": 17,
+            "duration": 2,
+            "color": "#46615e",
+            "task": "Task #1"
+        }, {
+            "duration": 2,
+            "color": "#727d6f",
+            "task": "Task #2"
+        }, {
+            "duration": 2,
+            "color": "#8dc49f",
+            "task": "Task #3"
+        } ]
+    }, {
+        "category": "Aircraf2",
+        "segments": [ {
+            "start": 13,
+            "duration": 2,
+            "color": "#727d6f",
+            "task": "Task #2"
+        }, {
+            "duration": 1,
+            "color": "#8dc49f",
+            "task": "Task #3"
+        }, {
+            "duration": 4,
+            "color": "#46615e",
+            "task": "Task #1"
+        } ]
+    }, {
+        "category": "Aircraf2",
+        "segments": [ {
+            "start": 40,
+            "duration": 3,
+            "color": "#727d6f",
+            "task": "Task #2"
+        }, {
+            "start": 17,
+            "duration": 4,
+            "color": "#FFE4C4",
+            "task": "Task #4"
+        } ]
+    } ]
+#data for the risk (working-predicted cycles) plot
+histogram_data = [{
+        "name": 'Working',
+        "data": [49.9, 71.5, 106.4, 129.2, 144.0]
+
+    }, {
+
+        "name": 'Predicted until failure',
+        "data": [83.6, 78.8, 98.5, 93.4, 106.0]
+
+    }, {
+        "name": 'Total',
+        "data": [48.9, 38.8, 39.3, 41.4, 47.0]
+
+    }]
+#data for the failure chance on dust plot
+dust_data =[
+        [1, 14.3, 27.7, 21.5],
+        [2, 14.5, 27.8 , 22.1],
+        [2, 15.5, 29.6, 20],
+        [3, 16.7, 30.7, 20],
+        [3, 16.5, 25.0, 21],
+        [5, 17.8, 25.7, 18],
+        [6, 13.5, 24.8, 23],
+        [6, 10.5, 21.4, 12],
+        [8, 9.2, 23.8, 10],
+        [12, 11.6, 21.8,15],
+        [15, 10.7, 23.7,16],
+        [15, 11.0, 23.3,17] ]
 
 
 @app.route("/")
 def main():
-    print("main")
     return render_template('main_screen.html')
 
-@app.route('/dataGraph', methods=['GET', 'POST'])
-def upload_file():
-        print("upload")
-        if request.method == 'GET':
-            sensorValues=["1", "0.0023", "0.0003", "100.0", "518.67", "643.02", "1585.29", "1398.21", "14.62", "21.61", "553.9", "2388.04",
-                                "9050.17", "1.3", "47.2", "521.72", "2388.03", "8125.55", "8.4052", "0.03", "392", "2388", "100.0", "38.86",
-                                "23.3735", "0.0", "0.0","518.67", "643.02", "1585.29", "1398.21", "14.62", "21.61", "553.9", "2388.04",
-                                "9050.17", "1.3", "47.2", "521.72", "2388.03", "8125.55", "8.4052", "0.03", "392", "2388", "100.0", "38.86",
-                                "23.3735", "0.0", "0.0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0","0"]
-            # The form of this is defined in backendController class
+@app.route('/dustExposureGraph', methods=['GET', 'POST'])
+def first_tile_graph():
+    if request.method == 'GET':
+             return  jsonify(dust_exposure_data)
 
-            returnVal = getMultiClassPredictorForValues(sensorValues)
-            return str(returnVal)
+@app.route('/riskGraph', methods=['GET', 'POST'])
+def risk_graph_upload():
+    if request.method == 'GET':
+             return  jsonify(risk_graph_data)
 
+@app.route('/histogram', methods=['GET', 'POST'])
+def second_tile_graph():
+    if request.method == 'GET':
+             return  jsonify(histogram_data)
+
+@app.route('/dustVariation', methods=['GET', 'POST'])
+def third_tile_graph():
+    if request.method == 'GET':
+             return  jsonify(dust_data)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+     app.run(debug=True)
